b0a08320ac6e2f0c712eed4eecebe20ca5ad5fc2
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-09-01 00:09:16 +0900

    feat : study spring mvc,api

---
+++ src/main/java/study/hello/controller/HelloController.java
@@ -0,0 +1,53 @@
+package study.hello.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+
+@Controller
+public class HelloController {
+    // Static
+    @GetMapping("hello")
+    public String hello(Model model){
+        model.addAttribute("data","spring!!!");
+        return "hello";
+    }
+    // MVC
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name",required = false) String name, Model model) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+    // API
+    @GetMapping("hello-string")
+    @ResponseBody // http 통신 프로토콜 body 부에 데이터를 직접 넣어주겠단 의미
+    public String helloString(@RequestParam(value = "name", required = true) String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api")
+    @ResponseBody // http 통신 프로토콜 body 부에 데이터를 직접 넣어주겠단 의미
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        for (int i = 0; i < 10; i++) {
+            hello.setName(name + String.valueOf(i));
+        }
+        return hello;
+    }
+
+    static class Hello{
+        private ArrayList<String> nameList = new ArrayList<>();
+
+        public String getName() {
+            return nameList.toString();
+        }
+
+        public void setName(String name) {
+            this.nameList.add(name);
+        }
+    }
+}
---
+++ src/main/java/study/hello/mission1/service/MemberService.java
@@ -0,0 +1,53 @@
+package study.hello.mission1.service;
+
+import study.hello.mission1.domain.Member;
+import study.hello.mission1.repository.MemberRepository;
+import study.hello.mission1.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    /**
+     * 회원가입
+     * @param member
+     * @return id(=sequence)
+     */
+    public Long join(Member member){
+        validateDuplicateMember(member);    // 중복 회원 검증
+        memberRepository.save(member);
+        return member.getId();
+    }
+    // ctrl + T : Refactor This -> Extract Method
+    private void validateDuplicateMember(Member member) {
+        memberRepository.findByName(member.getName())
+                .ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다.");
+        });
+    }
+
+    /**
+     * 전체 회원 조회
+     * @return memberList
+     */
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    /**
+     * 특정 회원 조회 by id
+     * @param memberId
+     * @return Member
+     */
+    public Member findOne(Long memberId){
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new IllegalArgumentException("Member not found"));
+    }
+
+}
