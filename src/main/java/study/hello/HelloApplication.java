f9a55d41d53a7737151bcdff8792af95c46b6767
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-09-23 01:04:53 +0900

    feat : rename study.hello.Application.java

--- src/main/java/study/hello/HelloApplication.java
+++ src/main/java/study/hello/HelloApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
+public class HelloApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(HelloApplication.class, args);
 	}
 }
