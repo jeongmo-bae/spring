4e7f644b3abbd10d460739934af899556411217e
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-10-07 19:48:44 +0900

    study SpringBean LifeCycle

---
+++ src/test/java/study/lifecycle/NetworkClientV2.java
@@ -0,0 +1,42 @@
+package study.lifecycle;
+
+public class NetworkClientV2 {
+    private String url;
+
+    public NetworkClientV2() {
+        System.out.println("생성자 호출, url = " + url);
+//        connect();
+//        call("초기화 연결 메시지");
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    //서비스 시작시 호출
+    public void connect(){
+        System.out.println("connect : " + url );
+    }
+
+    public void call(String message){
+        System.out.println("call : " + url + " message = " + message);
+    }
+
+    //서비스 종료시 호출
+    public void disconnect(){
+        System.out.println("close : " + url);
+    }
+
+
+    public void init() {
+        System.out.println("NetworkClientV2.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+
+    public void close() {
+        System.out.println("NetworkClientV2.close");
+        disconnect();
+    }
+}
