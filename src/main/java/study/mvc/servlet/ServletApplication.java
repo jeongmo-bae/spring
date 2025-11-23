d6d580f730c532e902a505a69d6826ed6bd9756d
배정모 <jungmo@baejeongmoui-Macmini.local>
2025-11-24 00:39:25 +0900

    Hello Servlet

---
+++ src/main/java/study/mvc/servlet/ServletApplication.java
@@ -0,0 +1,15 @@
+package study.mvc.servlet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@SpringBootApplication
+public class ServletApplication {
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(ServletApplication.class);
+        app.setAdditionalProfiles("spring-mvc-study-servlet");
+        app.run(args);
+    }
+}
