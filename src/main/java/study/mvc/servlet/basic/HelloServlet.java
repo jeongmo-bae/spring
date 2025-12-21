e1c362bd33edc7772fafa1cac482c96aa1da92b9
jeongmo-bae <jungmob1024@gmail.com>
2025-12-21 22:51:56 +0900

    study basic of HttpServletRequest

--- src/main/java/study/mvc/servlet/basic/HelloServlet.java
+++ src/main/java/study/mvc/servlet/basic/HelloServlet.java
@@ -10,17 +10,17 @@ import java.io.IOException;
 
 @WebServlet(name="helloServlet", urlPatterns = "/hello")
 public class HelloServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 //        super.service(request, response);
         System.out.println("HelloServlet.service");
-        System.out.println("request = " + request);
-        System.out.println("response = " + response);
+        System.out.println("request = " + request); //request = org.apache.catalina.connector.RequestFacade@3e82ed19
+        System.out.println("response = " + response); //response = org.apache.catalina.connector.ResponseFacade@3f59e116
         String username = request.getParameter("username");
         System.out.println("username = " + username);
 
         response.setContentType("text/plain");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write("Hello " + username);
-    }
-}
+        response.setCharacterEncoding("UTF-8"); // 이 두개는 헤더에 들어감
+        response.getWriter().write("Hello " + username); //http message body 에 데이터가 들어감
+    }}
